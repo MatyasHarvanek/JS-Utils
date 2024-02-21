1311575f22dab9efab68459f134e5706a040421a
MatyasHarvanek <matyas.harvanek@outlook.com>
2024-02-21 16:24:32 +0100

    first commit

---
+++ NavBarScrollHighlighter/NavBarScrollHighlighter.js
@@ -0,0 +1,58 @@
+/*
+ __  __                                    
+/\ \/\ \                             __    
+\ \ \_\ \      __     _ __   __  __ /\_\   
+ \ \  _  \   /'__`\  /\`'__\/\ \/\ \\/\ \  
+  \ \ \ \ \ /\ \L\.\_\ \ \/ \ \ \_/ |\ \ \ 
+   \ \_\ \_\\ \__/.\_\\ \_\  \ \___/  \ \_\
+    \/_/\/_/ \/__/\/_/ \/_/   \/__/    \/_/
+
+    gitHub: https://github.com/MatyasHarvanek
+*/
+
+var pages;
+var activeNavItem;
+var navItems;
+
+function initNBSH(pageClassName, navBarItemClassName) {
+    registerPages(pageClassName);
+    registerNavItems(navBarItemClassName);
+    onscroll();
+}
+
+function registerNavItems(className) {
+    navItems = document.getElementsByClassName(className);
+    if (navItems === null) {
+        console.log("Could not find any nav items with this class name!");
+    }
+}
+
+function registerPages(className) {
+    pages = document.getElementsByClassName(className);
+    if (pages === null) {
+        console.log("Could not find any pages with this calass name!");
+    }
+}
+
+function setActivePage(navItemIndex) {
+    if (activeNavItem) {
+        activeNavItem.classList.remove("active");
+    }
+    activeNavItem = navItems[navItemIndex];
+    activeNavItem.classList.add("active");
+}
+
+onscroll = () => {
+    for (let pageIndex = 0; pageIndex < pages.length; pageIndex++) {
+
+        var rect = pages[pageIndex].getBoundingClientRect();
+        var staticTop = rect.top + window.scrollY;
+        var staticBottom = rect.bottom + window.scrollY;
+        var centerOfView = window.scrollY + (window.innerHeight / 2);
+
+        if (centerOfView > staticTop && centerOfView < staticBottom) {
+            setActivePage(pageIndex);
+            break;
+        }
+    }
+};
---
+++ README.md
@@ -0,0 +1,34 @@
+
+![alt text](image.png)
+# Collection of JS utilities for websites 
+ * Ongoing project that contains JS scripts that can help with developing websites.
+<br />
+<br />
+<br />
+
+# Utilities
+
+## 🖱 NavBarScrollHighlighter (NBSH) 🖱
+
+![alt text](/mdImages/NBSH.gif)
+
+<br />
+
+* NBSH will highlight nav bar link depending on section of page where you are
+* Usefull for web presentation with one page layout
+<br />
+
+  ### Usage
+  
+  1. add *initNBSH(\<page class name\>, \<nav link class name\>)* to your JS script
+
+      ![alt text](/mdImages/NBSH-1.png)
+
+  2. structure of HTML nav bar
+
+      ![alt text](/mdImages/NBSH-2.png)
+
+  3. structure of HTML pages
+      
+      ![alt text](/mdImages/NBSH-3.png)
+
